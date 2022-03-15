fb4c2049a5a44cf681c7a01e19ba0276e3c15898
akash.wissen <akash.chowdhury@wissen.com>
2022-03-15 12:39:45 +0530

    change

--- Hokage.java
+++ Hokage.java
@@ -1,8 +1,8 @@
 public class Hokage{
 	public static void main(String args[]){
-	// Original data will be Kakashi Hatake
-	String data = "Kakashi Hatake";
-	String data2 = "Konoha";
+
 	System.out.println("Hi I'm Akash");
+
+	
 }
 }
