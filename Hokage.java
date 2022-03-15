ae058844bdf77fe15708e1561820cb901a902b71
akash.wissen <akash.chowdhury@wissen.com>
2022-03-15 12:51:45 +0530

    Added changes wrt dev2

--- Hokage.java
+++ Hokage.java
@@ -1,6 +1,7 @@
 public class Hokage{
 	public static void main(String args[]){
 
-	               System.out.println("Hi I'm Sasuke");
+	               System.out.println("Hi I'm Sasuke hhdhdhdhdh s");
+				   int i = 0;
 }
 }
