8ba4841c02d29c540f2be89660242794c272dc69
akash.wissen <akash.chowdhury@wissen.com>
2022-03-15 12:40:41 +0530

    naruto

--- Hokage.java
+++ Hokage.java
@@ -1,7 +1,6 @@
 public class Hokage{
 	public static void main(String args[]){
-	String data = "Naruto Uzumaki";
-	String data2 = "Konoha";
-	System.out.println("Hi I'm Naruto");
+	
+	System.out.println("Hi I'm Naruto   ");
 }
 }
